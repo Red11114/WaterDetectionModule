39021fb9d33b7c1269f58be2fac977492bd894b3
Red11114 <56987716+Red11114@users.noreply.github.com>
2020-09-20 17:01:02 +0930

    Add files via upload

--- plot_battery.py
+++ plot_battery.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
 print("importing complete")
-fname='/home/andrew/Documents/WaterDetectionModule/21-3-2020:23-8-30_power.txt'
+fname='/home/andrew/Documents/WaterDetectionModule/21-3-2020:23-53-35_power.txt'
 
 data = {
     "time" : [],
@@ -24,22 +24,30 @@ with open(fname, "r") as f:
             data["current"].append(float(info[2]))
             # print(line)
 
-major_ticks = []
-for i in data["time"]:
-    times = i.split(":")
-    print(times)
-    if int(times[1]) % 10 == 0:
-        major_ticks.append(int(times[1]))
+# major_ticks = []
+# for i in data["time"]:
+#     times = i.split(":")
+#     print(times)
+#     if int(times[1]) % 10 == 0:
+#         major_ticks.append(int(times[1]))
 
 fig, ax = plt.subplots()
 ax.plot(data["time"],data["current"])
-ax.xaxis.set_major_locator(MultipleLocator(50))
+# ax.xaxis.set_major_locator(MultipleLocator(50))
 # ax.xaxis.set_major_formatter(FormatStrFormatter('%s:%s:%s'))
 plt.xticks(rotation='vertical')
 plt.subplots_adjust(bottom=0.3)
 # ax.xaxis.set_horizontalalignment('right')
-plt.show()
-plt.savefig('Current vs Time.png')
 
+# plt.savefig('Current vs Time.png')
+
+fig2, ax2 = plt.subplots()
+ax2.plot(data["time"],data["voltage"])
+# ax2.xaxis.set_major_locator(MultipleLocator(50))
+# ax.xaxis.set_major_formatter(FormatStrFormatter('%s:%s:%s'))
+plt.xticks(rotation='vertical')
+plt.subplots_adjust(bottom=0.3)
+# ax.xaxis.set_horizontalalignment('right')
 # print(data["time"])
 # print(data["voltage"])
+plt.show()
