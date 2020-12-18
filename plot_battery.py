0dc2753e67b792851e575071ade59ef945b012ff
andrew <andrew@veesix.cc>
2020-12-19 00:39:20 +1030

    plot battery now takes in normal log file

--- plot_battery.py
+++ plot_battery.py
@@ -1,9 +1,11 @@
 #!/bin/bash
+import datetime
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                 AutoMinorLocator)
 print("importing complete")
-fname='logs/27-11-2020:13-30-34_log.txt'
+fname='logs/14-12-2020_8-32-58.log'
 
 data = {
     "time" : [],
@@ -20,31 +22,36 @@ with open(fname, "r") as f:
         # else:
         if "DEBUG" in line:
             info = line.split("=")
+
             data["time"].append(info[0][:-11])
             data["voltage"].append(float(info[1][:-2]))
-            data["current"].append(float(info[2][:-1]))
+            data["current"].append(float(info[2][:-2]))
             # print(line)
 print("File complete")
 # major_ticks = []
-print(data)
+
+for i in range(len(data["time"])):
+    data['time'][i]= [datetime.datetime.strptime(data['time'][i][:-4],"%Y-%m-%d %H:%M:%S,")]
+
 # for i in data["time"]:
 #     times = i.split(":")
 #     print(times)
 #     if int(times[1]) % 10 == 0:
 #         major_ticks.append(int(times[1]))
+formatter = mdates.DateFormatter("%d %H:%M")
 
 fig, ax = plt.subplots()
 ax.plot(data["time"],data["current"])
 print(data["time"][1])
 num = len(data["time"])/10
-ax.xaxis.set_major_locator(MultipleLocator(num))
+ax.xaxis.set_major_formatter(formatter)
 # ax.xaxis.set_major_formatter(FormatStrFormatter('%s:%s:%s'))
 plt.xticks(rotation='vertical')
 plt.subplots_adjust(bottom=0.2)
 # ax.xaxis.set_horizontalalignment('right')
 print("Plot complete")
 
-plt.show()
+# plt.show()
 plt.savefig('Current_vs_Time.png')
 print("first done")
 fig2, ax2 = plt.subplots()
@@ -52,11 +59,13 @@ ax2.plot(data["time"],data["voltage"])
 
 print(data["time"][1])
 num = len(data["time"])/10
-ax2.xaxis.set_major_locator(MultipleLocator(num))
+# ax2.xaxis.set_major_locator(MultipleLocator(num))
+ax2.xaxis.set_major_formatter(formatter)
 # ax.xaxis.set_major_formatter(FormatStrFormatter('%s:%s:%s'))
 plt.xticks(rotation='vertical')
 plt.subplots_adjust(bottom=0.2)
 plt.savefig('Voltage_vs_Time.png')
+plt.show()
 print("SAVED")
 # fig2, ax2 = plt.subplots()
 # ax2.plot(data["time"],data["voltage"])
